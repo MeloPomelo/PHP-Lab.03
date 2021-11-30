e75503af60622aa68b81ae8ae896a894b5bc7ab8
Daniil <volkov.daniil.vit@ya.ru>
2021-11-30 17:14:35 +0500

    Create Point.php and Vector.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\MagicClass;
+use App\Point;
+use App\Vector;
 
 require 'vendor/autoload.php';
 
@@ -18,7 +20,6 @@ echo $magic-> hidden . "<br/>";
 isset($magic->a);
 unset($magic->a);
 
-
 echo $magic->__sleep() . "<br/>";
 echo $magic->__wakeup() . "<br/>";
 
@@ -26,7 +27,6 @@ echo $magic->__serialize() . "<br/>";
 echo $magic->__unserialize() . "<br/>";
 
 echo $magic;
-
 
 $magic(5);
 
@@ -37,4 +37,20 @@ $b = var_export($a, true);
 eval('$c = ' . $b . ';');
 
 var_dump($magic);
+echo "</pre>";
+
+echo "<pre>";
+$T1 = new Point(6,-9);
+$V1 = new Vector(2, -3);
+$V2 = new Vector(0, 0);
+$V3 = new Vector(-6, -4);
+
+echo $V1->get_length() . "<br/>";
+echo $V2->get_length() . "<br/>";
+echo $V3->get_length() . "<br/>";
+
+var_dump($V1->perpendicular($V2));
+
+$T1->move_point($V1->x, $V1->y);
+echo $T1;
 echo "</pre>";
